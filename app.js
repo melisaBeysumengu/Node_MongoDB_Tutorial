fe40c1a6a41b1e9d34f9095a4b0cd963f864cc28
Melisa Beysümengü <61505693+melisaBeysumengu@users.noreply.github.com>
2021-06-23 16:18:27 +0300

    initial

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+//app.set('views', 'myviews');
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+
+    //res.send('<p>HomePage</p>');
+    //res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index', { title: 'Home', blogs});
+
+});
+
+app.get('/about', (req, res) => {
+
+    //res.send('<p>AboutPage</p>');
+    //res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about', { title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog'});
+})
+
+// 404 page
+app.use((req, res) => {
+    //res.status(404).sendFile('./views/404.html', { root: __dirname})
+    res.status(404).render('404', { title: '404'});
+})
