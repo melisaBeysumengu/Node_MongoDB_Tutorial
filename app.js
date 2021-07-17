1f1b9dc28323e1d16dd13188b413d417ac0480c9
Melisa Beysümengü <61505693+melisaBeysumengu@users.noreply.github.com>
2021-07-18 01:27:06 +0300

    mvc improvements made, css handeled

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
+
+// connect to mongodb
+const dbURI='mongodb+srv://netninja:test1234@nodetutorial.6qu2v.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))// listen for requests
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine', 'ejs');
 //app.set('views', 'myviews');
 
-// listen for requests
-app.listen(3000);
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-
-    //res.send('<p>HomePage</p>');
-    //res.sendFile('./views/index.html', { root: __dirname});
-    res.render('index', { title: 'Home', blogs});
-
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-
-    //res.send('<p>AboutPage</p>');
-    //res.sendFile('./views/about.html', { root: __dirname});
     res.render('about', { title: 'About'});
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog'});
-})
+// blog routes
+app.use('/blogs', blogRoutes);
 
-// 404 page
+// 404 page........... use has to be at the bottom
 app.use((req, res) => {
     //res.status(404).sendFile('./views/404.html', { root: __dirname})
     res.status(404).render('404', { title: '404'});
